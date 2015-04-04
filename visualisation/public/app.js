2c4806ef347dd87c0bbdfa83e9db0ab2bcc72d21
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2015-04-05 02:04:53 +1100

    Change directory structure.

--- visualisation/public/app.js
+++ visualisation/public/app.js
@@ -6,15 +6,10 @@
 	'use strict';
 
 	requirejs.config({
-		// Load standard javascript files in the specified directory.
-		baseUrl: 'assets/javascripts',
-		// Indicate the namespace.
-		namespace: 'app',
 		// Specify the paths.
 		paths: {
-			app: 'app',
-			jquery: '../../bower_components/jquery/dist/jquery',
-			threejs: '../../bower_components/threejs/build/three'
+			jquery: '../bower_components/jquery/dist/jquery',
+			threejs: '../bower_components/threejs/build/three'
 		},
 		shim: {
 			'threejs': {
