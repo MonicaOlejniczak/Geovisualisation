bec1f4c0c9713a7520f55c4ba82afe75d58506b2
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2015-04-05 02:31:23 +1100

    Convert spaces to indents.

--- visualisation/public/app/scene.js
+++ visualisation/public/app/scene.js
@@ -3,84 +3,93 @@
  */
 define(['threejs'], function (THREE) {
 
-    'use strict';
+	'use strict';
 
-    /**
-     * Initialises the scene.
-     *
-     * @param [scene] The scene object.
-     * @constructor
-     */
-    var Scene = function (scene) {
-        this.scene = scene || new THREE.Scene();
-        this.sphereSize = 5;
-    };
+	/**
+	 * Initialises the scene.
+	 *
+	 * @param [scene] The scene object.
+	 * @constructor
+	 */
+	var Scene = function (scene) {
+		this.scene = scene || new THREE.Scene();
+		this.sphereSize = 5;
+	};
 
-    Scene.prototype.constructor = Scene;
+	Scene.prototype.constructor = Scene;
 
-    /**
-     * Adds a directional light to the scene given the options.
-     *
-     * @param options The configuration for the directional light.
-     * @returns {THREE.DirectionalLight} The directional light that was created.
-     */
-    Scene.prototype.addDirectionalLight = function (options) {
-        // Create the directional light
-        var directionalLight = new THREE.DirectionalLight(options.color || 0xffffff, options.intensity || 0.5);
-        // Get the position from the options
-        var position = options.position;
-        // Check of the position exists and set it
-        if (position) {
-            directionalLight.position.set(position.x, position.y, position.z);
-        }
-        // Get the target from the options
-        var target = options.target;
-        // Set the target of the directional light
-        directionalLight.target.position.set(target.x || 0, target.y || 0, target.z || 0);
-        // Check if the light is being displayed
-        if (options.display) {
-            // Create the point light helper and add it to the scene
-            this.scene.add(new THREE.DirectionalLightHelper(directionalLight, this.sphereSize));
-        }
-        // Add the directional light to the scene and return it
-        return this.add(directionalLight);
-    };
+	/**
+	 * A method that retrieves the threejs scene.
+	 *
+	 * @returns {*|THREE.Scene} The threejs scene.
+	 */
+	Scene.prototype.get = function () {
+		return this.scene;
+	};
 
-    /**
-     * Adds a point light to the scene given the options.
-     *
-     * @param options The configuration for the point light.
-     * @returns {THREE.PointLight} The point light that was created.
-     */
-    Scene.prototype.addPointLight = function (options) {
-        // Create the point light
-        var pointLight = new THREE.PointLight(options.color || 0xffffff, options.intensity || 1, options.distance || 0);
-        // Get the position from the options
-        var position = options.position;
-        // Check if the position exists and set it
-        if (position) {
-            pointLight.position.set(position.x, position.y, position.z);
-        }
-        // Check if the light is being displayed
-        if (options.display) {
-            // Create the point light helper and add it to the scene
-            this.scene.add(new THREE.PointLightHelper(pointLight, this.sphereSize));
-        }
-        // Add the point light to the scene and return it
-        return this.add(pointLight);
-    };
+	/**
+	 * Adds a directional light to the scene given the options.
+	 *
+	 * @param options The configuration for the directional light.
+	 * @returns {THREE.DirectionalLight} The directional light that was created.
+	 */
+	Scene.prototype.addDirectionalLight = function (options) {
+		// Create the directional light
+		var directionalLight = new THREE.DirectionalLight(options.color || 0xffffff, options.intensity || 0.5);
+		// Get the position from the options
+		var position = options.position;
+		// Check of the position exists and set it
+		if (position) {
+			directionalLight.position.set(position.x, position.y, position.z);
+		}
+		// Get the target from the options
+		var target = options.target;
+		// Set the target of the directional light
+		directionalLight.target.position.set(target.x || 0, target.y || 0, target.z || 0);
+		// Check if the light is being displayed
+		if (options.display) {
+			// Create the point light helper and add it to the scene
+			this.scene.add(new THREE.DirectionalLightHelper(directionalLight, this.sphereSize));
+		}
+		// Add the directional light to the scene and return it
+		return this.add(directionalLight);
+	};
 
-    /**
-     * Adds an object to the scene.
-     *
-     * @param object The object being added to the scene.
-     * @returns {*} The object that was added to the scene.
-     */
-    Scene.prototype.add = function (object) {
-        this.scene.add(object);
-        return object;
-    };
+	/**
+	 * Adds a point light to the scene given the options.
+	 *
+	 * @param options The configuration for the point light.
+	 * @returns {THREE.PointLight} The point light that was created.
+	 */
+	Scene.prototype.addPointLight = function (options) {
+		// Create the point light
+		var pointLight = new THREE.PointLight(options.color || 0xffffff, options.intensity || 1, options.distance || 0);
+		// Get the position from the options
+		var position = options.position;
+		// Check if the position exists and set it
+		if (position) {
+			pointLight.position.set(position.x, position.y, position.z);
+		}
+		// Check if the light is being displayed
+		if (options.display) {
+			// Create the point light helper and add it to the scene
+			this.scene.add(new THREE.PointLightHelper(pointLight, this.sphereSize));
+		}
+		// Add the point light to the scene and return it
+		return this.add(pointLight);
+	};
 
-    return Scene;
+	/**
+	 * Adds an object to the scene.
+	 *
+	 * @param object The object being added to the scene.
+	 * @returns {*} The object that was added to the scene.
+	 */
+	Scene.prototype.add = function (object) {
+		this.scene.add(object);
+		return object;
+	};
+
+	return Scene;
 
 });
--- visualisation/public/app/util/color.js
+++ visualisation/public/app/util/color.js
@@ -3,49 +3,49 @@
  */
 define(['THREE'], function (THREE) {
 
-    'use strict';
+	'use strict';
 
-    /**
-     * Initialises the color class.
-     *
-     * @constructor
-     */
-    var Color = function () {};
+	/**
+	 * Initialises the color class.
+	 *
+	 * @constructor
+	 */
+	var Color = function () {};
 
-    Color.prototype.constructor = Color;
+	Color.prototype.constructor = Color;
 
-    /**
-     * Generates a random colour, blended with a particular colour.
-     *
-     * @param blend The THREE.Color to blend with the randomly generated values.
-     * @returns {THREE.Color}
-     */
-    Color.generate = function (blend) {
-        var red = Math.random();
-        var green = Math.random();
-        var blue = Math.random();
-        if (blend != null) {
-            red = (red + blend.r) * 0.5;
-            green = (green + blend.g) * 0.5;
-            blue = (blue + blend.b) * 0.5;
-        }
-        return new THREE.Color(red, green, blue);
-    };
+	/**
+	 * Generates a random colour, blended with a particular colour.
+	 *
+	 * @param blend The THREE.Color to blend with the randomly generated values.
+	 * @returns {THREE.Color}
+	 */
+	Color.generate = function (blend) {
+		var red = Math.random();
+		var green = Math.random();
+		var blue = Math.random();
+		if (blend != null) {
+			red = (red + blend.r) * 0.5;
+			green = (green + blend.g) * 0.5;
+			blue = (blue + blend.b) * 0.5;
+		}
+		return new THREE.Color(red, green, blue);
+	};
 
-    /**
-     * Darkens or lightens a colour based on the luminance.
-     *
-     * @param color The colour being darkened or lightened.
-     * @param luminance The intensity of the luminance.
-     * @returns {THREE.Color}
-     */
-    Color.luminance = function (color, luminance) {
-        var red = Math.max(0, Math.min(1, color.r + luminance));
-        var green = Math.max(0, Math.min(1, color.g + luminance));
-        var blue = Math.max(0, Math.min(1, color.b + luminance));
-        return new THREE.Color(red, green, blue);
-    };
+	/**
+	 * Darkens or lightens a colour based on the luminance.
+	 *
+	 * @param color The colour being darkened or lightened.
+	 * @param luminance The intensity of the luminance.
+	 * @returns {THREE.Color}
+	 */
+	Color.luminance = function (color, luminance) {
+		var red = Math.max(0, Math.min(1, color.r + luminance));
+		var green = Math.max(0, Math.min(1, color.g + luminance));
+		var blue = Math.max(0, Math.min(1, color.b + luminance));
+		return new THREE.Color(red, green, blue);
+	};
 
-    return Color;
+	return Color;
 
 });
