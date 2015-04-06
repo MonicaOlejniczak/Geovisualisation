5fd2dfc0371e7f6240e15062e03f62895f3437d3
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2015-04-06 18:41:29 +1000

    Add comments to visualisation.

--- visualisation/public/app/view/visualisation.js
+++ visualisation/public/app/view/visualisation.js
@@ -16,46 +16,61 @@ define(['jquery', 'threejs', 'Canvas', 'util/Color'], function ($, THREE, Canvas
 			cameraPosition: new THREE.Vector3(0, 0, 50),
 			controls: true
 		});
-		this.readData();
-		// Create the elements in the scene
-		this.createScene(this.scene);
+		// Read the data from the test file.
+		this.readData('app/data/test.json');
+		// Render the scene.
 		this.render();
 	};
 
 	Visualisation.prototype = Object.create(Canvas.prototype);
 	Visualisation.prototype.constructor = Visualisation;
 
-	Visualisation.prototype.readData = function () {
-		$.getJSON('app/data/test.json', function (data) {
-			for (var i = 0, len = data.length; i < len; i++) {
-				this.addSeries(data[i]);
-			}
+	/**
+	 * Reads a data file given its url.
+	 *
+	 * @param url The url to read the data from.
+	 */
+	Visualisation.prototype.readData = function (url) {
+		// Make an asynchronous ajax request to retrieve the data from the specified url.
+		$.getJSON(url, function (data) {
+			// Process the data retrieved from the request.
+			this.processData(data);
 		}.bind(this));
 	};
 
-	Visualisation.prototype.addSeries = function (series) {
+	/**
+	 * Processes the data that contains the series information.
+	 *
+	 * @param data The data being processed.
+	 */
+	Visualisation.prototype.processData = function (data) {
+		// Iterates through every series.
+		for (var i = 0, len = data.length; i < len; i++) {
+			// Processes the series on the current iteration.
+			this.processSeries(data[i]);
+		}
+	};
+
+	/**
+	 * Processes the series data by iterating through each set of its points.
+	 *
+	 * @param series The series data being processed.
+	 */
+	Visualisation.prototype.processSeries = function (series) {
 		var geometry = new THREE.Geometry();
 		var material = new THREE.LineBasicMaterial({
 			color: Color.generate()
 		});
+		// Iterate through the series.
 		for (var i = 0, len = series.length; i < len; i++) {
+			// Retrieve the point from the series.
 			var point = series[i];
+			// Add the point to the vertices of the geometry.
 			geometry.vertices.push(new THREE.Vector3(point[0], point[1], 0));
 		}
+		// Create a line with the series data and add it to the scene.
 		var line = new THREE.Line(geometry, material);
 		this.scene.add(line);
-	};
-
-	/**
-	 * This method creates all the components inside the canvas and adds it to the scene.
-	 *
-	 * @param scene The scene object.
-	 */
-	Visualisation.prototype.createScene = function (scene) {
-		/*var geometry = new THREE.BoxGeometry(50, 50, 50);
-		var material = new THREE.MeshBasicMaterial();
-		var mesh = new THREE.Mesh(geometry, material);
-		scene.add(mesh);*/
 	};
 
 	return Visualisation;
