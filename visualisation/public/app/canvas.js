b9488fb757f5fcd0658ea9002f62936bf289ecd7
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2015-04-06 20:19:57 +1000

    Remove scene class and change data coordinates.

--- visualisation/public/app/canvas.js
+++ visualisation/public/app/canvas.js
@@ -4,7 +4,6 @@
 define(function (require) {
 
 	var THREE = require('threejs');
-	var Scene = require('Scene');
 	var Viewport = require('Viewport');
 
 	require('orbitcontrols');
@@ -22,22 +21,47 @@ define(function (require) {
 	 * @constructor
 	 */
 	var Canvas = function (element, options) {
+		// Set the canvas to the element.
 		canvas = element;
-		this.scene = new Scene();
+		// Initialise the scene.
+		this.scene = new THREE.Scene();
+		// Initialise the camera.
 		this.camera = new THREE.PerspectiveCamera(75, element.width / element.height, 1, 1000);
-		var cameraPosition = options.cameraPosition;
-		this.camera.position.set(cameraPosition.x || 0, cameraPosition.y || 0, cameraPosition.z || 0);
+		// Get the camera options.
+		var cameraOptions = options.camera;
+		// Check if the camera options exist.
+		if (cameraOptions) {
+			// Get the camera position from the camera options and check if it exists.
+			var cameraPosition = cameraOptions.position;
+			if (cameraPosition) {
+				// Set the camera position.
+				this.camera.position.set(cameraPosition.x, cameraPosition.y, cameraPosition.z);
+			}
+			// Get the up vector from the camera options and check if it exists.
+			var cameraUp = cameraOptions.up;
+			if (cameraUp) {
+				// Set the up vector of the camera.
+				this.camera.up.set(cameraUp.x, cameraUp.y, cameraUp.z);
+			}
+		}
+		// Save the viewport height and tan field of view values for resizing.
 		this.viewportHeight = viewport.getHeight();
 		this.tanFOV = Math.tan(THREE.Math.degToRad(this.camera.fov * 0.5));
+		// Initialise the WebGL renderer.
 		this.renderer = new THREE.WebGLRenderer({
 			canvas: element,
 			alpha: true,
 			antialias: true
 		});
+		this.sphereSize = options.sphereSize || 5;
+		// Check if axes are being added to the scene.
 		if (options.axes) {
+			// Add the axes to the scene.
 			this.scene.add(this.createAxes());
 		}
+		// Check if orbit controls are enabled.
 		if (options.controls) {
+			// Add the orbit controls.
 			this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
 		}
 	};
@@ -51,25 +75,25 @@ define(function (require) {
 	 * @returns {THREE.Object3D} The world axes.
 	 */
 	Canvas.prototype.createAxes = function (options) {
-		// Ensure the options exist
+		// Ensure the options exist.
 		options = options || {};
-		// Get the origin, target, length and axe colours from the options
+		// Get the origin, target, length and axe colours from the options.
 		var origin = options.origin || new THREE.Vector3(0, 0, 0);
 		var target = new THREE.Vector3(0, 0, 0);
 		var length = options.length || 1000;
 		var xColor = options.xColor || 0xFF0000;
 		var yColor = options.yColor || 0x00FF00;
 		var zColor = options.zColor || 0x0000FF;
-		// Create a container object for the axes
+		// Create a container object for the axes.
 		var axes = new THREE.Object3D();
-		// Build each X, Y and Z axis
+		// Build each X, Y and Z axis.
 		axes.add(this.createAxis(origin, target.clone().setX(length), xColor, false)); // +X
 		axes.add(this.createAxis(origin, target.clone().setX(-length), xColor, true)); // -X
 		axes.add(this.createAxis(origin, target.clone().setY(length), yColor, false)); // +Y
 		axes.add(this.createAxis(origin, target.clone().setY(-length), yColor, true)); // -Y
 		axes.add(this.createAxis(origin, target.clone().setZ(length), zColor, false)); // +Z
 		axes.add(this.createAxis(origin, target.clone().setZ(-length), zColor, true)); // -Z
-		// Return the axes
+		// Return the axes.
 		return axes;
 	};
 
@@ -83,9 +107,12 @@ define(function (require) {
 	 * @returns {THREE.Line}
 	 */
 	Canvas.prototype.createAxis = function (origin, target, color, dashed) {
+		// Create the geometry for the axis and define its material.
 		var geometry = new THREE.Geometry();
 		var material;
+		// Check if the axis is being displayed with a dashed material.
 		if (dashed) {
+			// Create the dashed material.
 			material = new THREE.LineDashedMaterial({
 				linewidth: 3,
 				color: color,
@@ -93,14 +120,18 @@ define(function (require) {
 				gapSize: 3
 			});
 		} else {
+			// Create the basic material.
 			material = new THREE.LineBasicMaterial({
 				linewidth: 3,
 				color: color
 			});
 		}
+		// Add the origin and target vector vertices.
 		geometry.vertices.push(origin.clone());
 		geometry.vertices.push(target.clone());
+		// Compute the distance between the origin and target.
 		geometry.computeLineDistances();
+		// Return the line representing the axis.
 		return new THREE.Line(geometry, material, THREE.LinePieces);
 	};
 
@@ -112,9 +143,9 @@ define(function (require) {
 	 * @param [replace] Whether the geometry is having its face vertex uvs replaced.
 	 */
 	Canvas.prototype.calculateTextureCoordinates = function (geometry, replace) {
-		// Compute the bounding box for the geometry
+		// Compute the bounding box for the geometry.
 		geometry.computeBoundingBox();
-		// Initialise the variables
+		// Initialise the variables.
 		var boundingBox = geometry.boundingBox,
 			max = boundingBox.max,
 			min = boundingBox.min,
@@ -123,38 +154,90 @@ define(function (require) {
 			vertices = geometry.vertices,
 			faces = geometry.faces,
 			coordinates = [];
-		// Iterate through every face in the geometry
+		// Iterate through every face in the geometry.
 		faces.forEach(function (face) {
-			// Get the vertices from the face
+			// Get the vertices from the face.
 			var v1 = vertices[face.a],
 				v2 = vertices[face.b],
 				v3 = vertices[face.c];
-			// Append the new texture coordinates
+			// Append the new texture coordinates.
 			coordinates.push([
 				new THREE.Vector2((v1.x + offset.x) / range.x, (v1.z + offset.z) / range.z),
 				new THREE.Vector2((v2.x + offset.x) / range.x, (v2.z + offset.z) / range.z),
 				new THREE.Vector2((v3.x + offset.x) / range.x, (v3.z + offset.z) / range.z)
 			]);
 		});
-		// Replace the face vertex coordinates if it is being replaced
+		// Replace the face vertex coordinates if it is being replaced.
 		if (replace) {
 			geometry.faceVertexUvs[0] = coordinates;
 		}
 	};
 
 	/**
+	 * Adds a directional light to the scene given the options.
+	 *
+	 * @param options The configuration for the directional light.
+	 * @returns {THREE.DirectionalLight} The directional light that was created.
+	 */
+	Canvas.prototype.addDirectionalLight = function (options) {
+		// Create the directional light.
+		var directionalLight = new THREE.DirectionalLight(options.color || 0xffffff, options.intensity || 0.5);
+		// Get the position from the options.
+		var position = options.position;
+		// Check if the position exists and set it.
+		if (position) {
+			directionalLight.position.set(position.x, position.y, position.z);
+		}
+		// Get the target from the options.
+		var target = options.target;
+		// Set the target of the directional light.
+		directionalLight.target.position.set(target.x || 0, target.y || 0, target.z || 0);
+		// Check if the helper is being displayed.
+		if (options.display) {
+			// Create the point light helper and add it to the scene.
+			this.scene.add(new THREE.DirectionalLightHelper(directionalLight, this.sphereSize));
+		}
+		// Add the directional light to the scene.
+		this.add(directionalLight);
+	};
+
+	/**
+	 * Adds a point light to the scene given the options.
+	 *
+	 * @param options The configuration for the point light.
+	 * @returns {THREE.PointLight} The point light that was created.
+	 */
+	Canvas.prototype.addPointLight = function (options) {
+		// Create the point light.
+		var pointLight = new THREE.PointLight(options.color || 0xffffff, options.intensity || 1, options.distance || 0);
+		// Get the position from the options.
+		var position = options.position;
+		// Check if the position exists and set it.
+		if (position) {
+			pointLight.position.set(position.x, position.y, position.z);
+		}
+		// Check if the helper is being displayed.
+		if (options.display) {
+			// Create the point light helper and add it to the scene.
+			this.scene.add(new THREE.PointLightHelper(pointLight, this.sphereSize));
+		}
+		// Add the point light to the scene.
+		this.add(pointLight);
+	};
+
+	/**
 	 * A method called on each render. This method updates the camera aspect ratio and the renderer display size.
 	 */
 	Canvas.prototype.resize = function () {
-		// Lookup the size the browser is displaying the canvas
+		// Lookup the size the browser is displaying the canvas.
 		var width = viewport.getWidth();
 		var height = viewport.getHeight();
-		// Check if the canvas is not the same size
+		// Check if the current size differs from the previous.
 		if (canvas.width != width || canvas.height != height) {
-			// Make the canvas the same size
+			// Make the canvas the same size.
 			canvas.width = width;
 			canvas.height = height;
-			// Update the camera aspect ratio, fov and the renderer size
+			// Update the camera aspect ratio, fov and the renderer size to match the new size.
 			this.camera.aspect = width / height;
 			this.camera.fov = (360 / Math.PI) * Math.atan(this.tanFOV * (height / this.viewportHeight));
 			this.camera.updateProjectionMatrix();
@@ -168,25 +251,7 @@ define(function (require) {
 	Canvas.prototype.render = function () {
 		this.resize();
 		requestAnimationFrame(this.render.bind(this));
-		this.renderer.render(this.getScene(), this.camera);
-	};
-
-	/**
-	 * An accessor method that returns the THREE.js scene.
-	 *
-	 * @returns {*} The scene object.
-	 */
-	Canvas.prototype.getScene = function () {
-		return this.scene.get();
-	};
-
-	/**
-	 * An accessor method that returns the private canvas element.
-	 *
-	 * @returns {*} The HTMl canvas that constructed this class.
-	 */
-	Canvas.prototype.getCanvas = function () {
-		return canvas;
+		this.renderer.render(this.scene, this.camera);
 	};
 
 	return Canvas;
--- visualisation/public/app/view/visualisation.js
+++ visualisation/public/app/view/visualisation.js
@@ -13,17 +13,35 @@ define(['jquery', 'threejs', 'Canvas', 'util/Color'], function ($, THREE, Canvas
 	 */
 	var Visualisation = function (canvas) {
 		Canvas.call(this, canvas.get(0), {
-			cameraPosition: new THREE.Vector3(0, 0, 50),
-			controls: true
+			camera: {
+				position: new THREE.Vector3(0, 10, 30) // The position of the camera.
+			},
+			controls: true                              // Enable orbit controls.
 		});
+		var scene = this.scene;
+		// Adds a grid to the scene.
+		scene.add(this.createGrid(50, 5));
 		// Read the data from the test file.
 		this.readData('app/data/test.json');
+		this.camera.lookAt(scene.position);
 		// Render the scene.
 		this.render();
 	};
 
 	Visualisation.prototype = Object.create(Canvas.prototype);
 	Visualisation.prototype.constructor = Visualisation;
+
+	/**
+	 * Creates and returns a grid.
+	 *
+	 * @param size The size of the grid.
+	 * @param step The step size for the grid. This is the amount of space between each line.
+	 * @returns {THREE.GridHelper} The grid object.
+	 */
+	Visualisation.prototype.createGrid = function (size, step) {
+		// Create and return the grid with the specified size and step size.
+		return new THREE.GridHelper(size, step);
+	};
 
 	/**
 	 * Reads a data file given its url.
@@ -57,20 +75,33 @@ define(['jquery', 'threejs', 'Canvas', 'util/Color'], function ($, THREE, Canvas
 	 * @param series The series data being processed.
 	 */
 	Visualisation.prototype.processSeries = function (series) {
-		var geometry = new THREE.Geometry();
-		var material = new THREE.LineBasicMaterial({
-			color: Color.generate()
-		});
+		var points = [];
 		// Iterate through the series.
 		for (var i = 0, len = series.length; i < len; i++) {
 			// Retrieve the point from the series.
 			var point = series[i];
 			// Add the point to the vertices of the geometry.
-			geometry.vertices.push(new THREE.Vector3(point[0], point[1], 0));
+			points.push(this.convertPoint(point));
 		}
-		// Create a line with the series data and add it to the scene.
+		// Create the curve using the points obtained from the series.
+		var curve = new THREE.SplineCurve3(points);
+		var geometry = new THREE.Geometry();
+		geometry.vertices = curve.getPoints(50);
+		var material = new THREE.LineBasicMaterial({
+			color: Color.generate()
+		});
 		var line = new THREE.Line(geometry, material);
 		this.scene.add(line);
+	};
+
+	/**
+	 * Converts a point into a vector with the correct 3D space.
+	 *
+	 * @param point The point being converted.
+	 * @returns {THREE.Vector3} The converted vector.
+	 */
+	Visualisation.prototype.convertPoint = function (point) {
+		return new THREE.Vector3(point[0], 0.01, -point[1]);
 	};
 
 	return Visualisation;
