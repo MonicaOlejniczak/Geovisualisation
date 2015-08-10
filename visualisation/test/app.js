bf9ddec20a9d5fd9f0b49e174ba029cc5d2a1ef5
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2015-08-10 22:13:12 +1000

    Add styling to the Mocha tests.

--- visualisation/test/app.js
+++ visualisation/test/app.js
@@ -11,7 +11,7 @@
 		// Specify the paths.
 		paths: {
 			app: '.',
-			tests: '../../tests',
+			test: '../../test',
 			chai: '../bower_components/chai/chai',
 			jquery: '../bower_components/jquery/dist/jquery',
 			text: '../bower_components/text/text',
@@ -25,7 +25,7 @@
 	mocha.setup('bdd');
 
 	require([
-		'tests/unit/ColorTest'
+		'test/unit/ColorTest'
 	], function () {
 		mocha.run();
 	});
