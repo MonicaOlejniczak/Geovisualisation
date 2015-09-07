5ac345e1bf46e536a63db00308b702726c6fcd60
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2015-09-07 20:28:19 +1000

    Change Information display to use Point model and make its template generic.

--- visualisation/public/app/view/information/Information.js
+++ visualisation/public/app/view/information/Information.js
@@ -6,6 +6,7 @@ define(function (require) {
 	'use strict';
 
 	var $ = require('jquery');
+	var THREE = require('threejs');
 	var Handlebars = require('handlebars');
 
 	var Point = require('component/points/point/Point');
@@ -35,33 +36,70 @@ define(function (require) {
 	 * Configures the Handlebars template by registering helpers and compiling the template.
 	 */
 	Information.prototype.configureTemplate = function () {
-		Handlebars.registerHelper('formatPosition', this.formatPosition.bind(this));
+		Handlebars.registerHelper('capitalize', this.capitalize.bind(this));
+		Handlebars.registerHelper('format', this.format.bind(this));
 		this.template = Handlebars.compile(Display);
 	};
 
 	/**
-	 * The function that formats the position.
+	 * Capitalizes the specified string.
 	 *
-	 * @param position The position being formatted.
-	 * @returns {string} The formatted position.
+	 * @param str The string to capitalize.
+	 * @returns {string}
 	 */
-	Information.prototype.formatPosition = function (position) {
-		var precision = 2;
-		return '[' +
-			this.formatNumber(position.x, precision) + ', ' +
-			this.formatNumber(position.y, precision) + ', ' +
-			this.formatNumber(position.z, precision) + ']';
+	Information.prototype.capitalize = function (str) {
+		return str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
+	};
+
+	/**
+	 * Formats a value based on its type.
+	 *
+	 * @param value The value to format.
+	 * @returns {*} The formatted value.
+	 */
+	Information.prototype.format = function (value) {
+		if (typeof value === 'number') {
+			return this.formatNumber(value);
+		} else if (typeof value === 'object') {
+			if (value instanceof THREE.Vector2 || value instanceof THREE.Vector3 || value instanceof THREE.Vector4) {
+				return this.formatVector(value);
+			}
+		}
+		return value;
+	};
+
+	/**
+	 * The function that formats a Vector.
+	 *
+	 * @param vector The vector being formatted.
+	 * @returns {string} The formatted vector.
+	 */
+	Information.prototype.formatVector = function (vector) {
+		// Initialise the str that will be returned.
+		var str = '[';
+		// Get the keys from the vector and its length.
+		var keys = Object.keys(vector);
+		var len = keys.length;
+		// Iterate through each key in the vector.
+		keys.forEach(function (key, index) {
+			// Append the formatted number to the string, followed by a ', ' if the current iteration is not at the
+			// last key.
+			str += this.formatNumber(vector[key]) + (index < len - 1 ? ', ' : '');
+		}, this);
+		// Return the string with a closing bracket.
+		return str + ']';
 	};
 
 	/**
 	 * Formats a number to a particular precision.
 	 *
 	 * @param value The number being formatted.
-	 * @param precision The precision being used.
+	 * @param [precision] The precision being used.
 	 * @returns {string|*}
 	 */
 	Information.prototype.formatNumber = function (value, precision) {
 		value = parseInt(value, 10);
+		precision = precision || 2;
 		return value.toFixed(precision);
 	};
 
@@ -80,7 +118,7 @@ define(function (require) {
 			var object = intersect.object;
 			// Only update the information if the object is is a point.
 			if (object instanceof Point) {
-				this.updateInformation(coordinates, object);
+				this.updateInformation(coordinates, object.model);
 			}
 		}, this);
 	};
@@ -100,10 +138,10 @@ define(function (require) {
 	 * Updates the information for an intersected object.
 	 *
 	 * @param coordinates The mouse coordinates of the raycast.
-	 * @param object The intersected object.
+	 * @param model The model of the intersected object.
 	 */
-	Information.prototype.updateInformation = function (coordinates, object) {
-		this.$el.html(this.template(object))
+	Information.prototype.updateInformation = function (coordinates, model) {
+		this.$el.html(this.template(model.toJSON()))
 			.css({
 				left: coordinates.x,
 				top: coordinates.y
